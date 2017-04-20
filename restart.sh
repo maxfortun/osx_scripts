730410bd1bc888d857551c172c7fcf51708711b9
Max <max@maxf.net>
2017-04-20 00:33:37 -0400

    Added more examples

--- restart.sh
+++ restart.sh
@@ -3,5 +3,35 @@
 # kAEReallyLogOut               = 'rlgo',
 # kAEShowRestartDialog          = 'rrst',
 # kAEShowShutdownDialog         = 'rsdn'
+
+#Shut down without showing a confirmation dialog:
+#osascript -e 'tell app "System Events" to shut down'
+#
+#Shut down after showing a confirmation dialog:
+#osascript -e 'tell app "loginwindow" to «event aevtrsdn»'
+#
+#Restart without showing a confirmation dialog:
+#osascript -e 'tell app "System Events" to restart'
+#
+#Restart after showing a confirmation dialog:
+#osascript -e 'tell app "loginwindow" to «event aevtrrst»'
+#
+#Log out without showing a confirmation dialog:
+#osascript -e 'tell app "System Events" to  «event aevtrlgo»'
+#
+#Log out after showing a confirmation dialog:
+#osascript -e 'tell app "System Events" to log out'
+
+#Go to sleep (pmset):
+#pmset sleepnow
+#
+#Go to sleep (AppleScript):
+#osascript -e 'tell app "System Events" to sleep'
+#
+#Put displays to sleep (10.9 and later):
+#pmset displaysleepnow
+
+
+
 osascript -e 'tell app "loginwindow" to "event aevtrrst"'
 
