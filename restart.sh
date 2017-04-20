6efb19f99d03c8fee7534397ab9be066f044ce61
Max <max@maxf.net>
2017-04-20 00:31:06 -0400

    Added more vents

--- restart.sh
+++ restart.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# kAELogOut                     = 'logo',
+# kAEReallyLogOut               = 'rlgo',
+# kAEShowRestartDialog          = 'rrst',
+# kAEShowShutdownDialog         = 'rsdn'
 osascript -e 'tell app "loginwindow" to "event aevtrrst"'
 
