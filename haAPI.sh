9b1311e1c59b1b45076ec3dad48301c587fe0551
Maxim Fortun <max@maxf.net>
2017-05-22 23:20:51 -0400

    Added dimming

--- haAPI.sh
+++ haAPI.sh
@@ -2,18 +2,27 @@
 
 if [ -z "$1" ]; then 
 	echo "Usage: $0 <api>"
-	echo "Usage: $0 <domain> <service> <id>"
+	echo "Usage: $0 <domain> <service> <id> [param:value] ..."
 	echo "e.g. $0 services"
-	echo "e.g. $0 light turn_off home_living_floor"
+	echo "e.g. $0 light turn_off home_living_floor brightness:30"
 fi
 
 if [ -n "$3" ]; then
 	domain=$1
-	service=$2
-	id=$3
+	shift
+	service=$1
+	shift
+	id=$1
+	shift
 	method=POST
 	api="services/$domain/$service"
-	data="{\"entity_id\": \"$domain.$id\"}"
+	data="{\"entity_id\": \"$domain.$id\""
+	for param in $*; do
+		name=${param%%:*}
+		value=${param##*:}
+		data="$data, \"$name\": \"$value\""
+	done
+	data="$data}"
 else
 	method=GET
 	api=$1
