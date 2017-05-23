9a0dbfc4885b5e218ea7ef7162812b24a4f9a87f
Maxim Fortun <max@maxf.net>
2017-05-22 22:18:04 -0400

    Added

--- installLaunchAgents.sh
+++ installLaunchAgents.sh
@@ -5,6 +5,7 @@ OFS=$IFS
 IFS=$(echo -en "\n\b")
 for file in LaunchAgents/$1*.plist; do
 	basename=$(basename $file)
+	echo "Installing $basename"
 	cp $file ~/Library/LaunchAgents/
 	launchctl unload -w ~/Library/LaunchAgents/$basename
 	launchctl load -w ~/Library/LaunchAgents/$basename
