b211f7d4996ae382167064f469ada1401f093eba
Max <max@maxf.net>
2017-10-23 22:33:00 -0400

    Added icalBuddy

--- kidsToSchool.sh
+++ kidsToSchool.sh
@@ -2,6 +2,18 @@
 
 cd $(dirname $0)
 
+# need this calendar: http://schools.nyc.gov/Calendar/default.htm#phone
+# https://calendar.google.com/calendar/ical/doecalendars%40gmail.com/public/basic.ics
+
+if [ ! -x icalBuddy ]; then
+	echo "This app relied on icalBuddy for school closure dates. icalBuddy is missing."
+	exit
+fi
+
+# check if school is closed, and exit if it is
+#./icalBuddy -ic "NYC Public School Calendar" eventsFrom:"Nov 23, 2017" to:"Nov 23, 2017"| grep -i school.*closed >/dev/null && exit 1
+./icalBuddy -ic "NYC Public School Calendar" eventsToday| grep -i school.*closed >/dev/null && exit 1
+
 ./receiverOn.sh
 
 prefixes=("Дети" "Повторяю")
@@ -10,7 +22,7 @@ for (( i=0; i<2; i++ )); do
 	if [ -n "${prefixes[$i]}" ]; then
 		prefix="${prefixes[$i]}"
 	fi
-	echo say -v Milena "$prefix, время одеваться и готовиться к выходу."
+	say -v Milena "$prefix, время одеваться и готовиться к выходу."
 	sleep 2
 done
 
