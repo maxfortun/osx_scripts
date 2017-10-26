1f3eade74dd80b6a2a81577a554522c3502af1e1
Max <max@maxf.net>
2017-10-26 14:04:18 -0400

    Added

---
+++ sayWeather.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd $(dirname $0)
+if [ ! -x node_modules/.bin/jp ]; then
+	if ! which npm >/dev/null; then
+		echo "NodeJS is required. Please install."
+		exit
+	fi
+	npm install jp-cli
+fi
+
+wanIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+loc=$(curl -s https://ipinfo.io/$wanIP/loc)
+api=$(curl -s -L https://api.weather.gov/points/$loc)
+url=$(echo "$api" | node_modules/.bin/jp properties.forecast | sed 's/^"//g;s/"$//g')
+curl -s $url | egrep '"name"|"detailedForecast"'|head -4|cut -d'"' -f4|xargs say
+
