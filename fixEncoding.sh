76404226e2b9df97e1910f347f4808ee468ac00a
Maxim Fortun <max@maxf.net>
2017-10-26 13:20:16 -0400

    Added

---
+++ fixEncoding.sh
@@ -0,0 +1,14 @@
+export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_ALL=
+
+set meta-flag on
+set input-meta on
+set output-meta on
+set convert-meta off
+
