2eef92fff60d9ee185adf90301847587eb55956d
Maxim Fortun <max@maxf.net>
2017-10-26 20:07:38 -0400

    Typo

--- installSpeakableItems.sh
+++ installSpeakableItems.sh
@@ -5,8 +5,8 @@ OFS=$IFS
 IFS=$(echo -en "\n\b")
 for file in Speakable\ Items/$1*.applescript; do
 	basename=$(basename $file .applescript)
-	osacompile -o ~/Library/Speach/Speakable\ Items/"$basename.scpt" "$file"
-	$SWD/registerSpeachCommand.sh ~/Library/Speach/Speakable\ Items/"$basename.scpt"
+	osacompile -o ~/Library/Speech/Speakable\ Items/"$basename.scpt" "$file"
+	$SWD/registerSpeachCommand.sh ~/Library/Speech/Speakable\ Items/"$basename.scpt"
 done
 IFS=$OFS
 
