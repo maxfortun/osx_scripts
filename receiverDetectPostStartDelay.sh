fe104cdc50f4da4216007f27e9cbd3c7ddb479a8
Maxim Fortun <max@maxf.net>
2017-11-14 10:48:07 -0500

    Reworked state tracking

--- receiverDetectPostStartDelay.sh
+++ receiverDetectPostStartDelay.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Listen to the first number you hear. That's the delay in seconds."
-./receiverOff.sh
-./receiverOn.sh
+./receiverPower.sh off
+./receiverPower.sh on
 start=$(date +"%s")
 for (( i=0; i<10; i++)); do
 	now=$(date +"%s")
